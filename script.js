72e8b89a12ed22d646b0acd64be302a91e3bc1d7
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-12-19 22:27:00 -0800

    add dynamic book adding

--- script.js
+++ script.js
@@ -12,7 +12,28 @@ function Book(author, title, pages){
 }
 function addBookToLibrary(book){
     //obviously I will need to edit this to display to the dom not just the console
-    myLibrary.push(book);
+    let bookObject = document.createElement("div")
+    bookObject.classList.add("book");
+
+    let titleString = document.createElement("div");
+    titleString.textContent = book.title;
+    titleString.classList.add("title");
+
+    let authorString = document.createElement("div");
+    authorString.textContent = book.author;
+    authorString.classList.add("author");
+
+    let pagesString = document.createElement("div");
+    pagesString.textContent = book.pages;
+    pagesString.classList.add("pages");
+
+    bookObject.appendChild(titleString);
+    bookObject.appendChild(authorString);
+    bookObject.appendChild(pagesString);
+
+    let display = document.getElementById("display");
+    display.appendChild(bookObject);
+
 }
 
 function displayLibrary(){
